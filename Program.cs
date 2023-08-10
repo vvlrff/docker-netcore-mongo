3f79bb15609be47e171a17becefe2f50ed1d8381
olegmarkelov <vvlrff@mail.ru>
2023-08-10 16:25:59 +0500

    add cors and change docker files

--- Program.cs
+++ Program.cs
@@ -12,20 +12,20 @@ using MongoDB.Driver;
 using System.Text;
 using System.Text.Json;
 
-//var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy(name: MyAllowSpecificOrigins,
-//                      policy =>
-//                      {
-//                          policy.WithOrigins("http://localhost:3000")
-//                            .AllowAnyHeader()
-//                            .AllowAnyMethod();
-//                      });
-//});
+builder.Services.AddCors(options =>
+{
+   options.AddPolicy(name: MyAllowSpecificOrigins,
+                     policy =>
+                     {
+                         policy.WithOrigins("http://localhost:3000")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                     });
+});
 
 // Add services to the container.
 BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
@@ -121,6 +121,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-//app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
